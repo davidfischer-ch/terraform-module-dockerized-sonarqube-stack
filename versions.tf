892339d506137a9248d18c271e36b32b91dcf568
David Fischer <david@fisch3r.net>
2024-02-11 22:19:23 +0100

    Uprade providers

--- versions.tf
+++ versions.tf
@@ -11,26 +11,26 @@ terraform {
     jinja = {
       # https://github.com/NikolaLohinski/terraform-provider-jinja/tags
       source  = "NikolaLohinski/jinja"
-      version = ">= 1.15.0"
+      version = ">= 1.17.0"
     }
 
     local = {
       # https://github.com/hashicorp/terraform-provider-local/tags
       source  = "hashicorp/local"
-      version = ">= 2.4.0"
+      version = ">= 2.4.1"
     }
 
     null = {
       # https://github.com/hashicorp/terraform-provider-null/tags
       # TODO replace by "terraform_data"
       source  = "hashicorp/null"
-      version = ">= 3.2.1"
+      version = ">= 3.2.2"
     }
 
     random = {
       # https://github.com/hashicorp/terraform-provider-random/tags
       source  = "hashicorp/random"
-      version = ">= 3.5.1"
+      version = ">= 3.6.0"
     }
   }
 }
