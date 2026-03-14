9e868a45c8474120135c867fc2c5b09d7db566ad
David Fischer <david@fisch3r.net>
2026-03-14 22:53:16 +0100

    Upgrade modules to latest versions in examples

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -20,7 +20,7 @@ resource "acme_certificate" "sonarqube" {
 }
 
 module "sonarqube" {
-  source = "git::https://github.com/davidfischer-ch/terraform-module-dockerized-sonarqube-stack.git?ref=1.1.0"
+  source = "git::https://github.com/davidfischer-ch/terraform-module-dockerized-sonarqube-stack.git?ref=1.1.2"
 
   identifier = "sonarqube"
 
