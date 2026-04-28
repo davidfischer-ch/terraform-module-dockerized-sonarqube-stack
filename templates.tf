587d3bd05e3433c9a52f1098f0713109e7ad9cd6
David Fischer <david@fisch3r.net>
2026-04-28 15:11:24 +0200

    Bump nginx-templates to v1.1.0

--- templates.tf
+++ templates.tf
@@ -1,3 +1,3 @@
 module "templates" {
-  source = "git::https://github.com/davidfischer-ch/terraform-module-nginx-templates.git?ref=1.0.0"
+  source = "git::https://github.com/davidfischer-ch/terraform-module-nginx-templates.git?ref=1.1.0"
 }
